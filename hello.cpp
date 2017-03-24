cfda55caa684d043c4c1fbae7125e832a23e29f4
DESKTOP-UBTIPPR\jkdo0923 <jkdo0923@gmail.com>
2017-03-24 17:19:33 +0900

    what..?

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void Hello(){
+	printf("Hello World!);
+}
+int main(void){
+	Hello();
+	printf("Hello World!);
+	printf("Hello World!);
+	printf("Hello World!);
+	printf("I am master");
+	printf("I am master branch");
+	return 0;
+}
